16b267f3d15554a4f46ac5e145cb2b4579d1ecb8
Siddharth Tripathi <siddharthtripathi3824@gmail.com>
2025-02-15 01:29:46 +0530

    Update urls.py

--- stocks/urls.py
+++ stocks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import stock_chart  # Import your view function
 
 urlpatterns = [
-    path('chart/', stock_chart, name='stock_chart'),
+    path('', stock_chart, name='stock_chart'),
 ]
 
 
